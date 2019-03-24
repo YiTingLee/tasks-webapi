45c5a7433a68ed559905b42f9fdaf6f6ee838ab4
Eric Lee <ranye0719@gmail.com>
2019-03-24 13:13:15 +0800

    add update API of users and tasks collections to project

--- src/index.js
+++ src/index.js
@@ -27,6 +27,27 @@ app.get('/user/:id', (req, res) => {
   }).catch(e => res.status(500).send())
 })
 
+app.patch('/user/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' });
+  }
+
+  try {
+    const user = await User.findOneAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+})
+
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body);
   task.save().then(() => res.send(task)).catch(e => res.status(500).send(e));
@@ -44,6 +65,27 @@ app.get('/task/:id', (req, res) => {
     }
     res.send(task);
   }).catch(e => res.status(500).send())
+})
+
+app.patch('/task/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['description', 'completed'];
+  const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' });
+  }
+
+  try {
+    const task = await Task.findOneAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 })
 
 app.listen(port, () => {
