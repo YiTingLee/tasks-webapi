dc2bf1fbd7c7892427f19d6d2d23040338fff616
Eric Lee <ranye0719@gmail.com>
2019-03-24 11:20:14 +0800

    add get and post API of users and tasks collections

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('./db/mongoose');
 const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,13 +10,41 @@ app.use(express.json());
 
 app.post('/users', (req, res) => {
   const user = new User(req.body);
-
-  user.save().then(() => {
-    res.send(user);
-  }).catch(e => {
-    res.status(400).send(e);
-  })
+  user.save().then(() => res.send(user)).catch(e => res.status(500).send(e));
 });
+
+app.get('/users', (req, res) => {
+  User.find({}).then(users => res.send(users)).catch(e => res.status(500).send(e));
+})
+
+app.get('/user/:id', (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id).then(user => {
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  }).catch(e => res.status(500).send())
+})
+
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body);
+  task.save().then(() => res.send(task)).catch(e => res.status(500).send(e));
+})
+
+app.get('/tasks', (req, res) => {
+  Task.find({}).then(tasks => res.send(tasks)).catch(e => res.status(500).send(e));
+})
+
+app.get('/task/:id', (req, res) => {
+  const _id = req.params.id;
+  Task.findById(_id).then(task => {
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  }).catch(e => res.status(500).send())
+})
 
 app.listen(port, () => {
   console.log('Server is up on port ' + port);
