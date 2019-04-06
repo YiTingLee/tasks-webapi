eb50985bcbe83fe79f3e18b83e64b7709607cd64
Eric Lee <ranye0719@gmail.com>
2019-04-06 10:28:53 +0800

    creating the logout method

--- src/routers/user.js
+++ src/routers/user.js
@@ -25,6 +25,27 @@ router.post('/users/login', async (req, res) => {
   }
 })
 
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(token => token.token !== req.token);
+    await req.user.save();
+
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
 router.get('/users', auth, (req, res) => {
   User.find({}).then(users => res.send(users)).catch(e => res.status(500).send(e));
 })
