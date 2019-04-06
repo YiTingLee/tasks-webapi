a6953c4ecd0c04a043fa8b42a849c67cca097769
Eric Lee <ranye0719@gmail.com>
2019-04-06 10:49:49 +0800

    adjust update and delete method

--- src/routers/user.js
+++ src/routers/user.js
@@ -46,7 +46,7 @@ router.post('/users/logoutAll', auth, async (req, res) => {
   }
 });
 
-router.get('/users', auth, (req, res) => {
+router.get('/users', (req, res) => {
   User.find({}).then(users => res.send(users)).catch(e => res.status(500).send(e));
 })
 
@@ -64,7 +64,7 @@ router.get('/users/:id', (req, res) => {
   }).catch(e => res.status(500).send())
 })
 
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ['name', 'email', 'password', 'age'];
   const isValidOperation = updates.every(update => allowedUpdates.includes(update));
@@ -74,29 +74,18 @@ router.patch('/users/:id', async (req, res) => {
   }
 
   try {
-    // const user = await User.findOneAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-    const user = await User.findById(req.params.id);
-    updates.forEach(update => user[update] = req.body[update]);
-    await user.save();
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    res.send(user);
+    updates.forEach(update => req.user[update] = req.body[update]);
+    await req.user.save();
+    res.send(req.user);
   } catch (e) {
     res.status(400).send(e);
   }
 })
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
-
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    res.send(user);
+    await req.user.remove();
+    res.send(req.user);
   } catch (e) {
     res.status(500).send();
   }
